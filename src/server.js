f4c74003ab7fab770d3a096e8a7811e87609ec9e
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2026-08-07 22:55:21 +0600

    refactor(backend): move server entry point to src/server.js and update configs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const { connectDB } = require('./src/config/db');
-const app = require('./src/app');
+const { connectDB } = require('./config/db');
+const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
